d69ad22aa2e03bc736fa2d293d936c9f1fc8d2ff
Nick <lunyak.kolya@mail.ru>
2017-10-26 22:21:34 +0300

    Property tests were moved to ExampleUnitTest

--- app/src/main/java/ru/cryhards/brootkiddie/MainActivity.kt
+++ app/src/main/java/ru/cryhards/brootkiddie/MainActivity.kt
@@ -2,31 +2,11 @@ package ru.cryhards.brootkiddie
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import ru.cryhards.brootkiddie.engine.util.Property
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // testing Property
-        val a = Property(5.0)
-        val b = Property(0.1)
-        val c = Property(3.2)
-
-        a + b - c   // returns b, but creates a chain of properties: a -> b -> c
-        /*
-            a - (b + c) + (d - e)
-            b --> c
-                  ^
-                  |
-                  a -> d -> e
-
-            returns d
-         */
-        a.value = 6.9
-
-        println(c.value ?: "lulz")
     }
 }
--- app/src/test/java/ru/cryhards/brootkiddie/ExampleUnitTest.kt
+++ app/src/test/java/ru/cryhards/brootkiddie/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package ru.cryhards.brootkiddie
 import org.junit.Test
 
 import org.junit.Assert.*
+import ru.cryhards.brootkiddie.engine.util.Property
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +12,28 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
+    fun testAddition() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun testProperty() {
+        val a = Property(5.0)
+        val b = Property(0.1)
+        val c = Property(3.2)
+
+        a + b - c   // returns b, but creates a chain of properties: a -> b -> c
+        /*
+            a - (b + c) + (d - e)
+            b --> c
+                  ^
+                  |
+                  a -> d -> e
+
+            returns d
+         */
+        a.value = 6.9
+
+        assertEquals(c.value, 6.9)
     }
 }
