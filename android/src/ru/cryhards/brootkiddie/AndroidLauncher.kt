ff6066a4cf601f521e33c60377e16aa6370c7d28
kisate <dhkas@mail.ru>
2018-01-21 23:35:48 +0300

    merged mechanics and random spawning

--- android/src/ru/cryhards/brootkiddie/AndroidLauncher.kt
+++ android/src/ru/cryhards/brootkiddie/AndroidLauncher.kt
@@ -1,14 +1,30 @@
 package ru.cryhards.brootkiddie
 
+import android.annotation.TargetApi
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 
 class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        hideSystemUi()
         super.onCreate(savedInstanceState)
         val config = AndroidApplicationConfiguration()
+
+        config.hideStatusBar = true
+        config.useImmersiveMode = true
         initialize(ReallyGame(), config)
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    fun hideSystemUi() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE)
     }
 }
--- core/src/ru/cryhards/brootkiddie/GlobalMapScreen.kt
+++ core/src/ru/cryhards/brootkiddie/GlobalMapScreen.kt
@@ -4,19 +4,32 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.*
 import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.utils.Align
 import com.badlogic.gdx.utils.viewport.FitViewport
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar.ProgressBarStyle
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
+import ru.cryhards.brootkiddie.malware.Distribution
+import ru.cryhards.brootkiddie.malware.MalwareExample
 
 
 /**
  * Created with love by luna_koly on 21.01.2018.
  */
 class GlobalMapScreen : Screen {
+
+    val player = Player()
+
+    val ticksInDay = 20
+
+    var ticksToNextDay = ticksInDay
+
+    lateinit var infectedLabel : Label
+
 
     private val cam = OrthographicCamera()  // width & height are not important due to FitViewport
     private val stage = Stage(FitViewport(
@@ -35,6 +48,16 @@ class GlobalMapScreen : Screen {
         stage.addActor(map)
 
 
+        val ftfg = FreeTypeFontGenerator(Gdx.files.internal("fonts/roboto.ttf"))
+        val par = FreeTypeFontGenerator.FreeTypeFontParameter()
+        par.size = 70
+
+        infectedLabel = Label("INFECTED : ${player.infectedNodes} DAY : ${player.days}", Label.LabelStyle(ftfg.generateFont(par), Color(0f, 0f, 0f, 1f)))
+        infectedLabel.x = 0f
+        infectedLabel.y = 0f
+
+        stage.addActor(infectedLabel)
+
         val stageAspect = stage.width / stage.height
         val mapAspect = map.width / map.height
 
@@ -45,6 +68,10 @@ class GlobalMapScreen : Screen {
         }
 
         map.setPosition(stage.width / 2, stage.height / 2, Align.center)
+
+        player.distribution = Distribution(player)
+        val malware = MalwareExample(player)
+        player.malwareList+=malware
 
     }
 
@@ -62,6 +89,7 @@ class GlobalMapScreen : Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
         stage.act(delta)
+        act(delta)
         stage.draw()
     }
 
@@ -81,4 +109,22 @@ class GlobalMapScreen : Screen {
         map.texture.dispose()
     }
 
+
+    fun act(deltaT: Float) {
+        ticksToNextDay-=1
+        if (ticksToNextDay == 0) {
+            player.days += 1
+            player.doDay()
+            updateUi()
+            ticksToNextDay = ticksInDay
+        }
+    }
+
+    fun calcInfectionSpeed(): Int {
+        return 1
+    }
+
+    private fun updateUi(){
+        infectedLabel.setText("INFECTED : ${player.infectedNodes} DAY : ${player.days}")
+    }
 }
