b572d070d88714dbd0c6f98fa78b2031962f85c5
Nick <lunyak.kolya@mail.ru>
2018-01-23 19:12:50 +0300

    Something about generation

---
+++ core/src/ru/cryhards/brootkiddie/GlobalMap.kt
@@ -0,0 +1,83 @@
+package ru.cryhards.brootkiddie
+
+import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.utils.Align
+
+/**
+ * Created with love by luna_koly on 23.01.2018.
+ */
+object GlobalMap {
+    val map = ImageActor("map.jpg")
+
+    fun highlightRegion(region: Regions): ImageActor {
+        val reg = Spawner.getRegionOverlappingSquare()
+        val pos = unmapCoordinates(region)
+        val size = unmapSize(region)
+
+        reg.setSize(size.first, size.second)
+        reg.setPosition(pos.first, pos.second, Align.center)
+        return reg
+    }
+
+    fun mapCoordinates(region: Actor): Pair<Float, Float> {
+        return Pair(
+                (region.x + region.width/2 - map.x - map.width/2) / (map.width/2),
+                (region.y + region.height/2 - map.y - map.height/2) / (map.height/2)
+        )
+    }
+
+    fun unmapCoordinates(region: Regions): Pair<Float, Float> {
+        return Pair(
+                map.x + map.width/2 + region.x * map.width/2,
+                map.y + map.height/2 + region.y * map.height/2
+        )
+    }
+
+    fun mapSize(region: Actor): Pair<Float, Float> {
+        return Pair(
+                region.width / map.width,
+                region.height / map.height
+        )
+    }
+
+    fun unmapSize(region: Regions): Pair<Float, Float> {
+        return Pair(
+                region.width * map.width,
+                region.height * map.height
+        )
+    }
+
+
+
+
+    enum class Regions(
+            val x: Float,
+            val y: Float,
+            val width: Float,
+            val height: Float) {
+
+        SIBERIA(0.6f, 0.7f, 0.2f, 0.245f),
+        EASTERN_RUSSIA(0.25f, 0.6f, 0.08f, 0.2f),
+        WESTERN_RUSSIA(0.85f, 0.7f, 0.12f, 0.17f),
+
+        USA(-0.55f, 0.227f, 0.14f, 0.12f),
+
+        EASTERN_CANADA(-0.64f, 0.6f, 0.138f, 0.24f),
+        WESTERN_CANADA(-0.42f, 0.44f, 0.103f, 0.17f),
+        NORTHERN_CANADA(-0.43f, 0.78f, 0.103f, 0.17f),
+
+        AUSTRALIA(0.74f, -0.54f, 0.12f, 0.17f),
+        GREENLAND(-0.21f, 0.83f, 0.1f, 0.16f),
+        BRASILIA(-0.28f, -0.39f, 0.07f, 0.16f),
+        ARGENTINA(-0.348f, -0.69f, 0.043f, 0.177f),
+
+        COLUMBIA(-0.357f, -0.227f, 0.079f, 0.093f),
+
+        NORTHERN_AFRICA(0.047f, -0.024f, 0.14f, 0.19f),
+        SOUTHERN_AFRICA(0.15f, -0.44f, 0.086f, 0.24f),
+        MADAGASCAR(0.26f, -0.48f, 0.028f, 0.09f),
+
+        CHINA(0.592f, 0.135f, 0.11f, 0.145f)
+    }
+
+}
