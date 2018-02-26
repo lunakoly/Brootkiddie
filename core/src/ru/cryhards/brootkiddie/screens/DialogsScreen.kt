8306bd9fa808eb0eaf26e040f10b3dbf7217c5df
testererererererer <testererererererer@yandex.ru>
2018-02-26 19:00:57 +0300

    Dialogs

--- core/src/ru/cryhards/brootkiddie/screens/DialogsScreen.kt
+++ core/src/ru/cryhards/brootkiddie/screens/DialogsScreen.kt
@@ -5,6 +5,7 @@ import com.badlogic.gdx.ScreenAdapter
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane
@@ -38,7 +39,7 @@ class DialogsScreen : ScreenAdapter() {
 
     private val dialogsGroup = HorizontalGroup()
 
-    private val emptyDialogsLabel = UI.StaticLabel("Looks like you have no dialogs at the moment. /n" +
+    private val emptyDialogsLabel = UI.StaticLabel("Looks like you have no dialogs at the moment.\n" +
             "Come back later")
 
     private val stage = Stage()
@@ -106,49 +107,78 @@ class DialogsScreen : ScreenAdapter() {
         dialogsBySender.clear()
         sendersGroup.clear()
 
-        for (d in Player.dialogs) {
-            if (!dialogsBySender.contains(d.sender)) {
-                dialogsBySender[d.sender] = mutableListOf(d)
-            } else {
-                dialogsBySender[d.sender]!!.add(d)
-            }
-        }
+        Gdx.app.log("DD", Player.senders.size.toString())
 
-        for (s in dialogsBySender.keys) {
-            val sender = Player.senders[s]
-            val image = ImageActor(sender!!)
-            image.setSize(50f, 50f)
+        if (!Player.senders.isEmpty()) {
 
-            val nameLabel = UI.StaticLabel(s)
+            emptyDialogsLabel.isVisible = false
 
-            val group = HorizontalGroup()
-            group.addActor(image)
-            group.addActor(nameLabel)
-
-            group.rowAlign(Align.center)
-
-            group.pack()
-
-            sendersGroup.addActor(group)
-        }
-
-        sendersGroup.pack()
-
-        for (dialog in Player.dialogs)
-        {
-            val button = UI.StaticTextButton(dialog.id)
-            Gdx.app.log("Dialog", dialog.id)
-            /**button.addListener(object : ClickListener(){
-                override fun touchUp(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int) {
-                    dialogWidget.dialog = dialog
-                    dialogWidget.refreshInfo()
+            for (d in Player.dialogs) {
+                if (!dialogsBySender.contains(d.sender)) {
+                    dialogsBySender[d.sender] = mutableListOf(d)
+                } else {
+                    dialogsBySender[d.sender]!!.add(d)
                 }
-            })**/
-            dialogsGroup.addActor(button)
+            }
+
+            for (s in dialogsBySender.keys) {
+                val sender = Player.senders[s]
+                val image = ImageActor(sender!!)
+                image.setSize(50f, 50f)
+
+                val nameLabel = UI.StaticLabel(s)
+
+                val group = HorizontalGroup()
+                group.addActor(image)
+                group.addActor(nameLabel)
+
+                group.rowAlign(Align.center)
+
+                group.pack()
+
+                group.addListener(object : InputListener() {
+                    override fun touchUp(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int) {
+                        showSender(sender)
+                    }
+                })
+
+                sendersGroup.addActor(group)
+            }
+
+            sendersGroup.pack()
+
+
+
+            for (dialog in Player.dialogs) {
+                val button = UI.StaticTextButton(dialog.id)
+                Gdx.app.log("Dialog", dialog.id)
+                /**button.addListener(object : ClickListener(){
+                override fun touchUp(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int) {
+                dialogWidget.dialog = dialog
+                dialogWidget.refreshInfo()
+                }
+                })**/
+                dialogsGroup.addActor(button)
+            }
+
+            uiGroup.pack()
+
+            showSender(dialogsBySender.keys.elementAt(0))
+        } else {
+            emptyDialogsLabel.isVisible = true
+            emptyDialogsLabel.setPosition(Gdx.graphics.width / 2f, Gdx.graphics.height / 2f, Align.center)
+            stage.addActor(emptyDialogsLabel)
         }
+    }
+
+    fun showSender(sender: String) {
+        dialogsGroup.clear()
+        val dialogs = dialogsBySender[sender]
+        dialogs!!
+                .map { UI.StaticTextButton(it.id) }
+                .forEach { dialogsGroup.addActor(it) }
 
         dialogsGroup.pack()
-        uiGroup.pack()
     }
 
     override fun show() {
